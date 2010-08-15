31abbd45af60a4f42187e2ea24fb8a45a7fe3c49
Michael Bleigh <michael@intridea.com>
2010-08-15 00:52:44 -0500

    Wow.

--- server.coffee
+++ server.coffee
@@ -1,4 +1,4 @@
-require 'sys'
+sys = require 'sys'
 require.paths.unshift('vendor/.npm/.cache/express/1.0.0rc/package/lib')
 require.paths.unshift('vendor/.npm/.cache/connect/0.2.3/package/lib')
 
@@ -8,4 +8,4 @@ app.get "/", (req,res)-> res.send "Hello world"
 
 app.listen parseInt(process.env.PORT) || 8000
 
-puts "Up and running on #{process.env.PORT || 8000}..."
+sys.puts "Up and running on #{process.env.PORT || 8000}..."
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 (function() {
-  var app;
-  require('sys');
+  var app, sys;
+  sys = require('sys');
   require.paths.unshift('vendor/.npm/.cache/express/1.0.0rc/package/lib');
   require.paths.unshift('vendor/.npm/.cache/connect/0.2.3/package/lib');
   app = require("express").createServer();
@@ -8,5 +8,5 @@
     return res.send("Hello world");
   });
   app.listen(parseInt(process.env.PORT) || 8000);
-  puts(("Up and running on " + (process.env.PORT || 8000) + "..."));
+  sys.puts(("Up and running on " + (process.env.PORT || 8000) + "..."));
 })();
