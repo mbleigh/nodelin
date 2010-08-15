fd110a7a8f25003cf997df636650b8b1dbfc6fb7
Michael Bleigh <michael@intridea.com>
2010-08-15 00:43:19 -0500

    Lots of pushes, huh.

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 (function() {
   var app;
+  require('sys');
   require.paths.unshift('vendor/.npm/.cache/express/1.0.0rc/package/lib');
-  require.paths.unshift('vendor/.npm/.cache/connect/0.2.3/package/lib');
+  require.paths.unshift('vendor/  .npm/.cache/connect/0.2.3/package/lib');
   app = require("express").createServer();
   app.get("/", function(req, res) {
     return res.send("Hello world");
