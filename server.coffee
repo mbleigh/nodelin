d597df3a92ec3767d435e6ad2ed15de100dfaae4
Michael Bleigh <michael@intridea.com>
2010-08-15 20:13:46 -0500

    Fixed Redis to be lamely manual.

--- server.coffee
+++ server.coffee
@@ -5,20 +5,20 @@ require.paths.unshift('vendor/.npm/.cache/connect/0.2.3/package/lib')
 app = require("express").createServer()
 
 # Get Redis up and running.
-[REDIS_PASS, REDIS_HOST, REDIS_PORT] = 
+[REDIS_PASS, REDIS_HOST, REDIS_PORT] = [process.env.REDIS_PASS, 'goosefish.redistogo.com', 9256]
  
-# redis = require("./vendor/redis/redis-client").createClient(REDIS_PORT, REDIS_HOST)
-# redis.auth(REDIS_PASS) if REDIS_PASS
+redis = require("./vendor/redis/redis-client").createClient(REDIS_PORT, REDIS_HOST)
+redis.auth(REDIS_PASS) if REDIS_PASS
 
 app.get "/", (req,res)-> 
-  # redis.get 'test', (err, val)->
-  res.send "Hello world #{process.env.REDISTOGO_URL}"
+  redis.get 'test', (err, val)->
+    res.send "Running for #{parseInt(val) * 5} seconds."
 
 app.listen parseInt(process.env.PORT) || 8000
 
 sys.puts "Up and running on #{process.env.PORT || 8000}..."
 
-# incrementor = ->
-#   redis.incr('test')
-#   
-# setInterval incrementor, 5000
+incrementor = ->
+  redis.incr('test')
+  
+setInterval incrementor, 5000
--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 (function() {
-  var REDIS_HOST, REDIS_PASS, REDIS_PORT, _a, app, sys;
+  var REDIS_HOST, REDIS_PASS, REDIS_PORT, _a, app, incrementor, redis, sys;
   sys = require('sys');
   require.paths.unshift('vendor/.npm/.cache/express/1.0.0rc/package/lib');
   require.paths.unshift('vendor/.npm/.cache/connect/0.2.3/package/lib');
   app = require("express").createServer();
-  _a = app.get("/", function(req, res) {
-    return res.send(("Hello world " + (process.env.REDISTOGO_URL)));
-  });
+  _a = [process.env.REDIS_PASS, 'goosefish.redistogo.com', 9256];
   REDIS_PASS = _a[0];
   REDIS_HOST = _a[1];
   REDIS_PORT = _a[2];
+  redis = require("./vendor/redis/redis-client").createClient(REDIS_PORT, REDIS_HOST);
+  if (REDIS_PASS) {
+    redis.auth(REDIS_PASS);
+  }
+  app.get("/", function(req, res) {
+    return redis.get('test', function(err, val) {
+      return res.send(("Running for " + (parseInt(val) * 5) + " seconds."));
+    });
+  });
   app.listen(parseInt(process.env.PORT) || 8000);
   sys.puts(("Up and running on " + (process.env.PORT || 8000) + "..."));
+  incrementor = function() {
+    return redis.incr('test');
+  };
+  setInterval(incrementor, 5000);
 })();
